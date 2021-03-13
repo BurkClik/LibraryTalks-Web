71377f9f86efc66884f7c47db4140d72baa67e01
BurkClik <burk.clik@gmail.com>
2021-03-13 23:51:02 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <h1>Bitcoin Price Index</h1>
+
+  <section v-if="errored">
+    <p>We're sorry, we're not able to retrieve this information at
+      the moment, please try back later</p>
+  </section>
+
+  <section v-else>
+    <div v-if="loading">Loading...</div>
+
+    <div
+      v-else
+      v-for="book in info"
+      class="book"
+      v-bind:key="book"
+    >
+      {{ book.title }}:
+      <span class="lighten">
+        {{ book.page }}
+      </span>
+    </div>
+
+  </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false,
+    };
+  },
+  mounted() {
+    axios
+      .get('http://localhost:1234/books')
+      .then((response: any) => {
+        this.info = response.data;
+      })
+      .catch((error: any) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => { this.loading = false; });
+  },
+})
+export default class Home extends Vue {}
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="signup">
+      <form action="">
+          <label for="fname">Full Name</label><br>
+          <input type="text" id="fname" name="fname" class="rounded-md"><br>
+          <label for="email">Email</label><br>
+          <input type="text" id="email" name="email" class="rounded-md"><br>
+          <label for="password">Password</label><br>
+          <input type="password" id="password" name="password" class="rounded-md"><br>
+          <button class="bg-green-600 m-3 py-2 px-6 rounded-2xl
+          text-white shadow-md">Signup</button>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
